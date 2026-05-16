0b57cf92b2c8c1641fc0f10e70c3801abe9c42d7
Neha <jknehvardhini222106619@gmail.com>
2026-05-16 11:30:50 +0530

    DB FIXED 01

--- database.py
+++ database.py
@@ -1,8 +1,24 @@
-from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy import create_engine
+# db_url="postgresql://postgres:LIAKAAHE@localhost/neha"
+# engine=create_engine(db_url)
+# SessionLocal=sessionmaker(autocommit=False,
+#                           autoflush=False,
+#                           bind=engine)
+# #
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
-db_url="postgresql://postgres:LIAKAAHE@localhost/neha"
-engine=create_engine(db_url)
-SessionLocal=sessionmaker(autocommit=False,
-                          autoflush=False,
-                          bind=engine)
-#
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
