2a3a77cca84d78fe3c3a6f865f5452a368efdc7b
Neha <jknehvardhini222106619@gmail.com>
2026-05-16 09:47:55 +0530

    Configured Neon database

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-db_url="postgresql://postgres:LIAKAAHE@localhost:5432/neha"
-engine=create_engine(db_url)
+# db_url="postgresql://postgres:LIAKAAHE@localhost:5432/neha"
+DATABASE_URL = "postgresql://neondb_owner:npg_cL4DSP0OMZzg@ep-rough-glade-ao914z9c.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
+
+engine=create_engine(DATABASE_URL)
 SessionLocal=sessionmaker(autocommit=False,
                           autoflush=False,
                           bind=engine)
